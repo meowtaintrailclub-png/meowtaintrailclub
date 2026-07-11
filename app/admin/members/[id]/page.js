ff57d25e55232c1e275d76a5d6d0421628c21ad1
meowtaintrailclub-png <meowtaintrailclub@gmail.com>
2026-07-11 18:14:16 +0800

    Update page.js

--- app/admin/members/[id]/page.js
+++ app/admin/members/[id]/page.js
@@ -1,4 +1,4 @@
-javascriptimport { redirect, notFound } from "next/navigation";
+import { redirect, notFound } from "next/navigation";
 import { supabaseAdmin } from "../../../../lib/supabase";
 import { isAdminLoggedIn } from "../../../../lib/adminSession";
 
