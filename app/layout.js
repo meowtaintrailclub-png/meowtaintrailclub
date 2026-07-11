1329d1fbca5d2e5cc048810b752540d36f5ff587
meowtaintrailclub-png <meowtaintrailclub@gmail.com>
2026-07-11 17:56:38 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 export const metadata = {
   title: "Meowtain Trail Club",
   description: "Monthly trail running challenge leaderboard",
+  icons: {
+    icon: "/Meowtain-logo.jpeg",
+  },
 };
 
 export default function RootLayout({ children }) {
