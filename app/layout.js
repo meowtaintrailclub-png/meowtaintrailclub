bb3c3124a0ba88121c35f3240cfd65fd7acba453
meowtaintrailclub-png <meowtaintrailclub@gmail.com>
2026-07-11 22:41:21 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -27,6 +27,10 @@ export default function RootLayout({ children }) {
           &copy; {year} Meowtain Trail Club. All rights reserved. &middot;{" "}
           <a href="/legal" style={{ color: "#8A8A85", textDecoration: "underline" }}>
             Privacy &amp; Disclaimer
+          </a>{" "}
+          &middot;{" "}
+          <a href="mailto:meowtaintrailclub@gmail.com" style={{ color: "#8A8A85", textDecoration: "underline" }}>
+            meowtaintrailclub@gmail.com
           </a>
         </footer>
       </body>
